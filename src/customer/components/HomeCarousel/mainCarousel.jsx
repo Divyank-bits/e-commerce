010cb70bf8b52d34d4edcbdf29686d004877c5f6
Divyank Vyas <divyankvyas00@gmail.com>
2024-10-21 22:09:19 +0530

    project setup and added navbar, carousel

---
+++ src/customer/components/HomeCarousel/mainCarousel.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+import {mainCarouselData}  from './mainCarouselData'
+
+
+const MainCarousel = () => {
+    const items = mainCarouselData.map((item)=> <img src={item.image} className='cursor-pointer' role='presentation' />)
+
+    return (
+        <div>
+        <AliceCarousel
+            mouseTracking
+            items={items}
+        />
+        </div>
+    )
+}
+
+export default MainCarousel;
---
+++ src/customer/pages/HomePage/homePage.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import MainCarousel from "../../components/HomeCarousel/mainCarousel"
+
+const HomePage = ()=> {
+    return (
+        <div>
+            <MainCarousel/>
+            <div>
+                Other Section
+            </div>
+        </div>
+    )
+}
+export default HomePage
